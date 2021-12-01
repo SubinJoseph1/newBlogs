62e98151040b98a254b6cbf44f2b3acbc734e421
Subin J <subin.j@idp.com>
2021-12-01 12:29:54 +0530

    'error'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
 
 
 const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
@@ -8,8 +9,13 @@ const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/hom
  { path: 'blog', loadChildren: () => import('./pages/blog/blog.module').then(m => m.BlogModule) },
  { path: 'addblog', loadChildren: () => import('./pages/addblog/addblog.module').then(m => m.AddblogModule) },
  //{ path: '', loadChildren: () => import('./pages/addblog/addblog.module').then(m => m.AddblogModule) },
-  {path:'profile',loadChildren:()=>import('./pages/profilemodule/profilemodule.module').then(m=>m.ProfilemoduleModule)}
+  {path:'profile',loadChildren:()=>import('./pages/profilemodule/profilemodule.module').then(m=>m.ProfilemoduleModule)},
 //{ path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
+{
+  path:'**',
+  component:ErrorComponent
+
+}
  ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
